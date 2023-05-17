fb06844e2411768be0d08eeb51db09f3372571a0
Dark Tornado <dt3141592@naver.com>
2023-05-17 23:37:09 +0900

    Update MainActivity.kt

--- java/com/darktornado/floatingbrightnesschanger/MainActivity.kt
+++ java/com/darktornado/floatingbrightnesschanger/MainActivity.kt
@@ -2,9 +2,14 @@ package com.darktornado.floatingbrightnesschanger
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val layout = LinearLayout(this)
+        layout.orientation = 1
+
+        setContentView(layout)
     }
 }
