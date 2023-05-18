4429b6b3226e25c3352f76aedae958051cbebd79
Dark Tornado <dt3141592@naver.com>
2023-05-19 00:59:44 +0900

    Update MainActivity.kt

--- java/com/darktornado/floatingbrightnesschanger/MainActivity.kt
+++ java/com/darktornado/floatingbrightnesschanger/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.darktornado.floatingbrightnesschanger
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.Switch
+
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,11 @@ class MainActivity : Activity() {
         layout.addView(on)
         val perm1 = Button(this)
         perm1.text = "다른 앱 위에 그리기 권한 허용"
+        perm1.setOnClickListener{
+            val uri = Uri.parse("package:$packageName")
+            val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, uri)
+            startActivityForResult(intent, 5469)
+        }
         layout.addView(perm1)
         val perm2 = Button(this)
         perm2.text = "시스템 설정 변경 권한 허용"
