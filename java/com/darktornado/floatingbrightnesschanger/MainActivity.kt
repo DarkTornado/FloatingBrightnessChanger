abb61ce2fadcaef1f83f7f24cf5d385249d4ba1b
Dark Tornado <dt3141592@naver.com>
2023-05-18 23:42:27 +0900

    main ui

--- java/com/darktornado/floatingbrightnesschanger/MainActivity.kt
+++ java/com/darktornado/floatingbrightnesschanger/MainActivity.kt
@@ -2,14 +2,24 @@ package com.darktornado.floatingbrightnesschanger
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Switch
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val layout = LinearLayout(this)
         layout.orientation = 1
-
+        val on = Switch(this)
+        on.text = "구석 터치 사용"
+        layout.addView(on)
+        val perm1 = Button(this)
+        perm1.text = "다른 앱 위에 그리기 권한 허용"
+        layout.addView(perm1)
+        val perm2 = Button(this)
+        perm2.text = "시스템 설정 변경 권한 허용"
+        layout.addView(perm2)
         setContentView(layout)
     }
 }
