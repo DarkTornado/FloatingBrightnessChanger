1abcc3c94b85281dfa9b0ecd5376329fe0f6f073
Dark Tornado <dt3141592@naver.com>
2023-05-29 01:42:58 +0900

    bug fixed

--- java/com/darktornado/floatingbrightnesschanger/MainService.kt
+++ java/com/darktornado/floatingbrightnesschanger/MainService.kt
@@ -1,4 +1,12 @@
 package com.darktornado.floatingbrightnesschanger
 
-class MainService {
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MainService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+//        TODO("Not yet implemented")
+    }
+
 }
