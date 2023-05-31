b2534f3102387b5bc12b8fe134b746ff0b8e6403
Dark Tornado <dt3141592@naver.com>
2023-06-01 03:55:23 +0900

    Update MainService.kt

--- java/com/darktornado/floatingbrightnesschanger/MainService.kt
+++ java/com/darktornado/floatingbrightnesschanger/MainService.kt
@@ -5,6 +5,12 @@ import android.content.Intent
 import android.os.IBinder
 
 class MainService : Service() {
+    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+
+        return START_NOT_STICKY
+    }
+
+
     override fun onBind(p0: Intent?): IBinder? {
 //        TODO("Not yet implemented")
         return null
